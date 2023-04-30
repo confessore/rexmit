1d751b42723484cb9aab2b2ba799b5e0df174ad2
steven confessore <confessore@balasolu.com>
2023-04-30 11:49:50 -0400

    refactoring

--- src/database.rs
+++ src/database.rs
@@ -253,21 +253,91 @@ pub async fn get_guilds_joined_to_channel() -> Option<Vec<String>> {
     return None;
 }
 
+/// gets a guild queue in mongo given a guild id
+///
+/// ### arguments
+/// 
+/// * `guild_id` - the discord issued id for the guild
+/// 
+/// ### returns 
+/// 
+/// some vector of string track urls
+/// 
 pub async fn get_guild_queue(guild_id: String) -> Option<Vec<String>> {
-    let collection_option = get_guild_collection().await;
-    if collection_option.is_some() {
-        println!("{}", "collection is some");
-        let collection = collection_option.unwrap();
-        let filter = doc! { "id": guild_id };
-        let guild_option_result = collection.find_one(filter, None).await;
-        if guild_option_result.is_ok() {
-            let guild_option = guild_option_result.unwrap();
-            if guild_option.is_some() {
-                let guild = guild_option.unwrap();
-                println!("{}", "cursor result is ok");
-                return Some(guild.queue);
+    let guild_collection_option = get_guild_collection().await;
+    match guild_collection_option {
+        Some(guild_collection) => {
+            println!("guild collection option is some");
+            let filter = doc! { "id": &guild_id };
+            let guild_option_result = guild_collection.find_one(filter, None).await;
+            match guild_option_result {
+                Ok(guild_option) => {
+                    println!("guild option result is ok");
+                    match guild_option {
+                        Some(guild) => {
+                            println!("guild option is some");
+                            return Some(guild.queue)
+                        }, 
+                        None => {
+                            println!("guild option is none");
+                            insert_new_guild(guild_id).await;
+                            return None;
+                        }
+                    }
+                },
+                Err(why) => {
+                    println!("guild option result is err");
+                    println!("{}", why);
+                    return None;
+                }
             }
+        },
+        None => {
+            println!("guild collection option is none");
+            return None;
         }
     }
-    return None;
+}
+
+/// sets a guild queue in mongo given a guild id and a queue
+///
+/// ### arguments
+/// 
+/// * `guild_id` - the discord issued id for the guild
+/// * `queue` - the vector of string track urls 
+/// 
+/// ### returns 
+/// 
+/// some vector of string track urls
+/// 
+pub async fn set_guild_queue(guild_id: String, queue: Vec<String>) -> Option<Vec<String>> {
+    let guild_collection_option = get_guild_collection().await;
+    match guild_collection_option {
+        Some(guild_collection) => {
+            let filter = doc! { "id": &guild_id };
+            let update = doc! { "$set": { "queue": &queue }};
+            let guild_option_result = guild_collection.find_one_and_update(filter, update, None).await;
+            match guild_option_result {
+                Ok(guild_option) => {
+                    match guild_option {
+                        Some(guild) => {
+                            println!("{:?}", guild);
+                            return Some(guild.queue)
+                        }, 
+                        None => {
+                            insert_new_guild(guild_id).await;
+                            return None;
+                        }
+                    }
+                },
+                Err(why) => {
+                    println!("{}", why);
+                    return None;
+                }
+            }
+        },
+        None => {
+            return None;
+        }
+    }
 }
